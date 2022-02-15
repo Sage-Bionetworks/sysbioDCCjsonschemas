e62c8a5990a2c6729e86805fd091bd78623ebb96
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-02-15 13:56:32 -0800

    fix typo

--- code/scripts/update-ad-dictionary-table.sh
+++ code/scripts/update-ad-dictionary-table.sh
@@ -8,7 +8,7 @@ syn = synapse.Synapse()
 # if using auth token:
 # syn.login(authToken = os.environ['SYNAPSE_PAT'], rememberMe = True)
 # if using .synapseConfig:
-synlogin(rememberMe = True)
+syn.login(rememberMe = True)
 exit()
 ## run script to update AD dccvalidator dictionary table
 python3 code/python/create_Syn_table_from_Syn_schemas.py \
