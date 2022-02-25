b9fbc9c6d790fe33d4bac6d6e606197eb3329ba8
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-02-24 17:08:31 -0800

    new line at end

--- code/scripts/update-ad-dictionary-table.sh
+++ code/scripts/update-ad-dictionary-table.sh
@@ -6,3 +6,4 @@ python3 code/python/synapse_login.py
 # run script to update AD dccvalidator dictionary table
 python3 code/python/create_Syn_table_from_Syn_schemas.py \
     --config_file config/schemas.yml --consortium AD overwrite_table --table_synapse_id syn21459391
+    
