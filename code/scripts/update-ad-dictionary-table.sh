aafda21fda2ac1af77a056df2f3408c01075ac0d
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-02-24 16:43:28 -0800

    run login before dictionary

--- code/scripts/update-ad-dictionary-table.sh
+++ code/scripts/update-ad-dictionary-table.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-python3
-## Login to synapse
-import os
-import synapseclient as synapse
-syn = synapse.Synapse()
-# if using auth token:
-# syn.login(authToken = os.environ['SYNAPSE_PAT'], rememberMe = True)
-# if using .synapseConfig:
-syn.login(rememberMe = True)
-exit()
-## run script to update AD dccvalidator dictionary table
+# run login script with scheduled job secrets
+python3 code/python/synapse_login.py
+
+# run script to update AD dccvalidator dictionary table
 python3 code/python/create_Syn_table_from_Syn_schemas.py \
     --config_file config/schemas.yml --consortium AD overwrite_table --table_synapse_id syn21459391
