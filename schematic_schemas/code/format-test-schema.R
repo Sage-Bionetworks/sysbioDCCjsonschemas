f02b0fc672e8fb258654aa498a34799dddf53113
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-09-21 20:12:56 -0700

    make test schema with individual, biospecimen, MRI, and rnaSeq attributes

--- schematic_schemas/code/format-test-schema.R
+++ schematic_schemas/code/format-test-schema.R
@@ -1,1 +1,33 @@
-#test commit
+library(here)
+library(tidyverse)
+library(glue)
+
+# read in large csv file that Anthony made
+big_model <- read_csv(here("schematic_schemas", "amp.ad.data.model.csv"))
+
+all_templates <- big_model %>% 
+  filter(Parent == "DataType") %>% 
+  select(Attribute)
+
+# pull out just a few templates to focus on
+subset_templates <- all_templates %>% 
+  filter(str_detect(Attribute, "assay_rnaSeq|assay_MRI|individual_animal|biospecimen_metadata")) %>% 
+  pull()
+
+# get all "dependsOn" values for this subset as a vector
+data_properties <- big_model %>% 
+  filter(Attribute %in% subset_templates) %>% 
+  pull(DependsOn) %>% 
+  glue_collapse(sep = ", ") %>% 
+  str_split(", ") %>% 
+  unlist() %>% 
+  unique()
+
+# filter data model for the 4 templates and their dependsOn attributes
+test_schema <- big_model %>% 
+  filter(Attribute %in% subset_templates | Attribute %in% data_properties)
+
+# write csv
+write_csv(test_schema, here("schematic_schemas", "model-ad-schematic-test-schema-v1.csv"), na = "")
+
+  
